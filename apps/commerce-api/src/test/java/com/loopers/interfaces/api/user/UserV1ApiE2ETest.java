a37d7ef7a056ab1cf8bb96a9c6a6a63e1d5a376e
simbokyung <synk98@gmail.com>
2025-07-16 13:05:58 +0900

    test : 내 정보 조회 통합, e2e 테스트 작성

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
@@ -1,6 +1,9 @@
 package com.loopers.interfaces.api.user;
 
+import com.loopers.domain.user.UserModel;
+import com.loopers.domain.user.UserRepository;
 import com.loopers.interfaces.api.ApiResponse;
+import com.loopers.interfaces.api.User.Gender;
 import com.loopers.interfaces.api.User.UserV1Dto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,22 +15,27 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserV1ApiE2ETest {
 
-    /**
-     * - [ ]  회원 가입이 성공할 경우, 생성된 유저 정보를 응답으로 반환한다.
-     * - [ ]  회원 가입 시에 성별이 없을 경우, `400 Bad Request` 응답을 반환한다.
-     */
 
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @DisplayName("POST /api/v1/users")
     @Nested
     class SignIn {
+        /**
+         * - [v]  회원 가입이 성공할 경우, 생성된 유저 정보를 응답으로 반환한다.
+         * - [v]  회원 가입 시에 성별이 없을 경우, `400 Bad Request` 응답을 반환한다.
+         */
+
+
         private static final String ENDPOINT_POST = "/api/v1/users";
         @DisplayName("회원 가입이 성공할 경우, 생성된 유저 정보를 응답으로 반환한다.")
         @Test
@@ -46,7 +54,6 @@ public class UserV1ApiE2ETest {
 
             //act
             ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {};
-
             ResponseEntity<ApiResponse< UserV1Dto.UserResponse>> response =
                     testRestTemplate.exchange(ENDPOINT_POST, HttpMethod.POST,  new HttpEntity<>(signInRequest, headers), responseType);
 
@@ -89,4 +96,68 @@ public class UserV1ApiE2ETest {
             );
         }
     }
+
+    @DisplayName("Get /api/v1/user/me")
+    @Nested
+    class Get {
+
+        /**
+         * - [ ]  내 정보 조회에 성공할 경우, 해당하는 유저 정보를 응답으로 반환한다.
+         * - [ ]  존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.
+         */
+        private static final String ENDPOINT_GET = "/api/v1/users/me";
+
+        @DisplayName("내 정보 조회에 성공할 경우, 해당하는 유저 정보를 응답으로 반환한다.")
+        @Test
+        void  returnsUserInfo_whenUserIdExists(){
+            //arrange
+            UserModel signInModel =  new UserModel("testId", Gender.MALE.getCode(), "2024-05-22", "loopers@test.com");
+            userRepository.save(signInModel);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-USER-ID", "testId");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            //act
+            ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ApiResponse<UserV1Dto.UserResponse>> response =
+                    testRestTemplate.exchange(ENDPOINT_GET, HttpMethod.GET, new HttpEntity<>(headers), responseType);
+
+
+
+            //assert
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> assertThat(response.getBody().data().loginId()).isEqualTo(signInModel.getLoginId()),
+                    () -> assertThat(response.getBody().data().gender()).isEqualTo(signInModel.getGender()),
+                    () -> assertThat(response.getBody().data().brith()).isEqualTo(signInModel.getBrith()),
+                    () -> assertThat(response.getBody().data().email()).isEqualTo(signInModel.getEmail())
+            );
+        }
+
+        @DisplayName("존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.")
+        @Test
+        void  throwsBadRequest_whenUserIdDoesNotExist(){
+
+            //arrange
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-USER-ID", "testId");
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            //act
+            ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {};
+            ResponseEntity<ApiResponse<UserV1Dto.UserResponse>> response =
+                    testRestTemplate.exchange(ENDPOINT_GET, HttpMethod.GET, new HttpEntity<>(headers), responseType);
+
+
+            //assert
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST),
+                    () -> assertThat(response.getBody().meta().result()).isEqualTo(ApiResponse.Metadata.Result.FAIL)
+            );
+
+        }
+    }
+
 }
