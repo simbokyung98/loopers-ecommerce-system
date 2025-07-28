094e97c5606e15c83a3cbb7a9360c6d5d576290e
simbokyung <synk98@gmail.com>
2025-07-28 23:19:11 +0900

    feat : 브랜드 도메인 모델 생성

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/BrandModel.java
@@ -0,0 +1,28 @@
+package com.loopers.domain.product;
+
+import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "brand")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BrandModel extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String  name;
+
+    public BrandModel(String name){
+        if(name == null){
+            throw new CoreException(ErrorType.BAD_REQUEST, "브랜드의 이름은 null 일 수 없습니다.");
+        }
+        this.name = name;
+    }
+}
