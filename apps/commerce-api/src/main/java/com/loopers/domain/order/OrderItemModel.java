59613858c491b7f995ecb79c1f6597ae32d0324f
simbokyung <synk98@gmail.com>
2025-07-31 17:06:23 +0900

    modify : 주문 도메인 생성 validaton 구체화

--- apps/commerce-api/src/main/java/com/loopers/domain/order/OrderItemModel.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/OrderItemModel.java
@@ -41,12 +41,12 @@ public class OrderItemModel extends BaseEntity {
             throw new CoreException(ErrorType.BAD_REQUEST, "상품 이름 없이 주문아이템을 생성할 수 없습니다.");
         }
 
-        if(price == null){
-            throw new CoreException(ErrorType.BAD_REQUEST, "상품 가격 없이 주문아이템을 생성할 수 없습니다.");
+        if(price == null || price < 0){
+            throw new CoreException(ErrorType.BAD_REQUEST, "상품 가격 비어있거나 음수일 수 없습니다.");
         }
 
-        if(quantity == null){
-            throw new CoreException(ErrorType.BAD_REQUEST, "상품 주문 개수 없이 주문아이템을 생성할 수 없습니다.");
+        if(quantity == null || quantity <0){
+            throw new CoreException(ErrorType.BAD_REQUEST, "주문 개수는 비어있거나 음수일 수 없습니다.");
         }
 
         this.orderId = orderId;
