8928c87e5e1097b80afc2526194cc8d8af48299d
simbokyung <synk98@gmail.com>
2025-08-09 18:09:48 +0900

    fix : 좋아요 요구사항 url 과 통일화

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1ApiController.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/like/LikeV1ApiController.java
@@ -9,29 +9,29 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/likes")
+@RequestMapping("/api/v1/like")
 public class LikeV1ApiController implements LikeV1ApiSpec {
 
     private final LikeFacade likeFacade;
 
     @Override
-    @PostMapping("like")
-    public ApiResponse<Object> like(@RequestHeader(value = "X-USER-ID") Long userid, @RequestBody LikeV1Dto.LikeRequest likeRequest) {
-        LikeCriteria.Like criteria = likeRequest.toCriteria(userid);
+    @PostMapping("/products/{productId}")
+    public ApiResponse<Object> like(@RequestHeader(value = "X-USER-ID") Long userid, @PathVariable Long productId) {
+        LikeCriteria.Like criteria = LikeCriteria.Like.of(userid, productId);
         likeFacade.like(criteria);
         return ApiResponse.success();
     }
 
     @Override
-    @PostMapping("/dislike")
-    public ApiResponse<Object> dislike(@RequestHeader(value = "X-USER-ID") Long userid, @RequestBody LikeV1Dto.DislikeRequest likeRequest) {
-        LikeCriteria.Dislike criteria = likeRequest.toCriteria(userid);
+    @DeleteMapping("/products/{productId}")
+    public ApiResponse<Object> dislike(@RequestHeader(value = "X-USER-ID") Long userid, @PathVariable Long productId) {
+        LikeCriteria.Dislike criteria = LikeCriteria.Dislike.of(userid, productId);
         likeFacade.dislike(criteria);
         return ApiResponse.success();
     }
 
     @Override
-    @GetMapping
+    @GetMapping("/products")
     public ApiResponse<LikeV1Dto.LikeProductsResponse> getLikedProducts(@RequestHeader(value = "X-USER-ID") Long userid) {
         LikeInfo.LikeProducts likeProducts = likeFacade.getLikedProducts(userid);
         return ApiResponse.success(LikeV1Dto.LikeProductsResponse.of(likeProducts));
