4a91ed537986b48245af0cd00ceb3bdee6ad7aa6
simbokyung <synk98@gmail.com>
2025-09-05 02:12:47 +0900

    add : 카프카 설정 연결

--- apps/commerce-api/build.gradle.kts
+++ apps/commerce-api/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     // add-ons
     implementation(project(":modules:jpa"))
     implementation(project(":modules:redis"))
+    implementation(project(":modules:kafka"))
     implementation(project(":supports:jackson"))
     implementation(project(":supports:logging"))
     implementation(project(":supports:monitoring"))
@@ -29,6 +30,7 @@ dependencies {
     // test-fixtures
     testImplementation(testFixtures(project(":modules:jpa")))
     testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation(testFixtures(project(":modules:kafka")))
     testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 
 
