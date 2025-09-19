7b2970018c91471682fd2190029de47dc5ab0d4d
simbokyung <synk98@gmail.com>
2025-09-19 10:54:08 +0900

    feat : 배치 모듈 생성

---
+++ apps/commerce-batch/build.gradle.kts
@@ -0,0 +1,29 @@
+dependencies {
+    // add-ons
+    implementation(project(":modules:jpa"))
+    implementation(project(":modules:redis"))
+    implementation(project(":supports:jackson"))
+    implementation(project(":supports:logging"))
+    implementation(project(":supports:monitoring"))
+
+    // web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation(project(mapOf("path" to ":apps:commerce-api")))
+
+
+    //batch
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+
+    // querydsl
+    annotationProcessor("com.querydsl:querydsl-apt::jakarta")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+
+    // test-fixtures
+    testImplementation(testFixtures(project(":modules:jpa")))
+    testImplementation(testFixtures(project(":modules:redis")))
+    testImplementation(testFixtures(project(":modules:kafka")))
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
+    testImplementation("org.springframework.batch:spring-batch-test")
+}
